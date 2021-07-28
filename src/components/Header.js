d60c6d4fcfeebcaab51506ca7ec43ac1cc20cf8d
sherryliao21 <sherry.c.liao@gmail.com>
2021-07-28 23:07:51 +0800

    feat: add temperature icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { FaTemperatureHigh } from 'react-icons/fa'
 
 const Header = () => {
   return (
     <div>
-      <h1>Temperature Converter</h1>
+      <h1 className="title">Temperature Converter<FaTemperatureHigh style={{ marginLeft: '20px'}}/></h1>
     </div>
   )
 }
